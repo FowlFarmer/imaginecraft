98d9e2ac99e5db7c0c01729569267c74c60470e5
Theodore Zhu <theodorez888@gmail.com>
2025-08-03 16:08:24 -0400

    pregen check

--- model_farm/pre_labelling.py
+++ model_farm/pre_labelling.py
@@ -16,6 +16,7 @@ stripped_dir = os.path.join(os.path.dirname(__file__), "image_bank/stripped")  #
 
 def generate_dataset():
     # Initialize models
+    pregenerated_filenames = [f for f in os.listdir(output_dir) if os.path.isfile(os.path.join(output_dir, f))]
     bg_remover = BackgroundRemover()
     img_generator = ImageGenerator()
     with open(input_dataset, 'r') as csvfile: # To optimize vram, do all imagen then do all rmbg
@@ -28,11 +29,15 @@ def generate_dataset():
             
             # Generate images
             for i in range(5):
-                generated_image = img_generator.generate_image(prompt)
-
                 output_name = os.path.join(output_dir, f"{name}_{str(i)}.png")
-                cv2.imwrite(output_name, generated_image)
-                print(f"Image saved as: {output_name}")
+                if output_name in pregenerated_filenames:
+                    print(f"Image {output_name} already exists, skipping generation.")
+                    continue
+                else:
+                    print(f"Generating image: {output_name}")
+                    generated_image = img_generator.generate_image(prompt)
+                    cv2.imwrite(output_name, generated_image)
+                    print(f"Image saved as: {output_name}")
 
     filenames = [f for f in os.listdir(output_dir) if os.path.isfile(os.path.join(output_dir, f))]
     for i in range(len(filenames)):
